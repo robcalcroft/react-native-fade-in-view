56811e79f6edc4d58e04b7868447ad9270b4525b
Rob Calcroft <rob.calcroft@opsview.com>
2017-01-31 14:37:46 +0000

    Initial setup

---
+++ FadeInView.js
@@ -0,0 +1,53 @@
+import React, { Component, PropTypes } from 'react';
+import { Animated } from 'react-native';
+
+class FadeInView extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      viewOpacity: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    const { viewOpacity } = this.state;
+    const { onFadeComplete, duration = 500 } = this.props;
+
+    Animated.timing(
+      viewOpacity,
+      {
+        toValue: 1,
+        duration,
+      },
+    ).start(onFadeComplete || (() => {}));
+  }
+
+  render() {
+    const { viewOpacity } = this.state;
+    const { style } = this.props;
+
+    return (
+      <Animated.View style={[{ opacity: viewOpacity }].concat(style || [])}>
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+
+FadeInView.propTypes = {
+  onFadeComplete: PropTypes.func,
+  duration: PropTypes.number,
+  style: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.array,
+  ]),
+  children: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object,
+  ]).isRequired,
+};
+
+export default FadeInView;
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # react-native-fade-in-view
-A simple and lightweight RN component that fades in its children
+> A simple and lightweight RN component that fades in its children
+
+## Install
+`yarn add react-native-fade-in-view` or `npm install react-native-fade-in-view --save`
+
+## API
+### `onFadeComplete`
+A function that is called when the fade animation completes
+
+### `duration || 500`
+The duration of the fade animation, 500ms by default
+
+### `style`
+Style to be given to the view
+
+## Usage
+```javascript
+import FadeInView from 'react-native-fade-in-view';
+
+const myFadeInComponent = () => (
+  <FadeInView
+    duration={750}
+    style={{ alignItems: 'center' }}
+    onFadeComplete={() => alert('Ready')}
+  >
+    <Text>This view will fade in nicely</Text>
+  </FadeInView>
+);
+```
